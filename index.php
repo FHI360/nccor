b6565ac77cb0c8686c67dfb9b834b871c4703e9a
Armen Danielyan <armen@Armens-MacBook-Pro.local>
2014-07-22 20:42:07 -0400

    added nccor template

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>NCCOR National Collaborative on Childhood Obesity Research</title>
+<meta name="description" content="The National Collaborative on Childhood Obesity Research (NCCOR) brings together four of the nation's leading research funders – the Centers for Disease Control and Prevention (CDC), the National Institutes of Health (NIH), the Robert Wood Johnson Foundation (RWJF), and the U.S. Department of Agriculture (USDA) – to address the problem of childhood obesity in America." />
+<script type="text/javascript" src="http://use.typekit.com/ofq3xyz.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+<link rel="shortcut icon" type="image/x-icon" href="http://nccor.org/images/favicon.ico" />
+
+<meta property="og:title" content="NCCOR" />
+<meta property="og:description" content="The National Collaborative on Childhood Obesity Research (NCCOR) brings together four of the nation's leading research funders – the Centers for Disease Control and Prevention (CDC), the National Institutes of Health (NIH), the Robert Wood Johnson Foundation (RWJF), and the U.S. Department of Agriculture (USDA) – to address the problem of childhood obesity in America." />
+
+ 
+<link href="../../css/redesign-2014/header-redesign-style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="http://www.google.com/cse/style/look/default.css" type="text/css" />
+<link href="../../css/global.css" rel="stylesheet" type="text/css" />
+<link href="../../css/dropdown.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="../../css/default.advanced.css" media="screen" rel="stylesheet" type="text/css" />
+<!--[if IE 6]><link href="../css/ie6-override.css" type="text/css" rel="stylesheet" media="screen" /><![endif]-->
+</head>
+
+<body>
+<?php include("../../includes/2014-preheader.php") ?>
+<div id="container">
+<div id="banner">
+
+<?php include("../../includes/2014-header.php") ?>
+<?php include("../../includes/2014-header-buttons.php") ?>
+
+  </div><!--end banner-->
+    
+    <div id="content">
+  
+        <div class="content-right">
+			
+            <p class="breadcrumbs">
+        	<a href="../../index.php" alt="Home">Home</a> &nbsp;&gt;&nbsp; <a href="../index.php" alt="Home">Tools</a> &nbsp;&gt;&nbsp; Project Map
+        	</p>
+            
+            <h1>Project Map</h1>
+            
+            <p>The Project Map is a searchable database of projects.</p>
+          	
+          <!-- the map -->
+
+          <!-- end the map -->
+
+</div><!--end content-right-->
+  </div><!--end content-->
+    
+<? include("../../includes/grand-footer.txt") ?>
+
+<? include("../../includes/footer.txt") ?>
+
+</div><!--end container-->
+
+
+<!--[if lt IE 7]>
+<script type="text/javascript" src="../../js/jquery/jquery.js"></script>
+<script type="text/javascript" src="../../js/jquery/jquery.dropdown.js"></script>
+<![endif]-->
+<script type="text/javascript" src="../../js/rollover.js"></script>
+<script type="text/javascript" src="../../js/search.js"></script>
+<!--LOAD Google Analytics Tracking of Outbound Clicks / Downloads -->
+    <script src="../../js/ga-tracking-downloads-outbound.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21178805-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
